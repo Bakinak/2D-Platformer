594f369d25b75ae518bfa3c598dfee1c23e66b14
Bakinak <akobbe16@student.aau.dk>
2020-07-12 00:09:36 +0200

    Animation controllers and testing health and robobot
    Idea for health: Make Canvas image. Have array of sprites indicating each health value in manager. Change sprite used in canvas each time you take damage.

---
+++ 2D Platformer/Assets/Scripts/Characters/RoboBotController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboBotController : MonoBehaviour
+{
+    public int animState = 0;
+    private Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {      
+        animator = GetComponent<Animator>();
+        animator.SetInteger("animState", animState);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeAnimation(int newState)
+    {
+        animator.SetInteger("animState", newState);
+    }
+
+}
---
+++ 2D Platformer/Assets/Scripts/myGameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myGameManager : MonoBehaviour
+{
+    public GameObject healthBar;
+    public int playerHealth;
+    private Animator healthAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthAnim = healthBar.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeHealth();
+    }
+
+    public void changeHealth()
+    {
+        healthAnim.SetInteger("health", playerHealth);
+    }
+
+}
