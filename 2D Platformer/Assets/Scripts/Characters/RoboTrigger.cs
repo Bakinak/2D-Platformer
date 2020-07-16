082726cdb1c24d467ed1ab36d52797d48d4eb03a
Bakinak <akobbe16@student.aau.dk>
2020-07-16 14:43:40 +0200

    Script to change robobot animations!

---
+++ 2D Platformer/Assets/Scripts/Characters/RoboTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboTrigger : MonoBehaviour
+{
+
+    GameObject roboBot;
+    RoboBotController controller;
+    public int onEnterChange =-1, onExitChange = -1;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roboBot = transform.parent.gameObject;
+        controller = roboBot.GetComponent<RoboBotController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (onEnterChange != -1) controller.changeAnimation(onEnterChange);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (onExitChange != -1) controller.changeAnimation(onExitChange);
+    }
+}
