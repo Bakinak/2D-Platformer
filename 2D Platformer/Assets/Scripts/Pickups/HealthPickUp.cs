d378f8541e66e8d99119f0758ca6b7eebeacba00
Bakinak <akobbe16@student.aau.dk>
2020-07-20 12:34:30 +0200

    Health Pickups work

---
+++ 2D Platformer/Assets/Scripts/Pickups/HealthPickUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickUp : PickUpClass
+{
+    public int healingAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void action(GameObject target)
+    {
+        target.GetComponent<Player>().gainHealth(healingAmount);
+        gameObject.SetActive(false);
+    }
+}
