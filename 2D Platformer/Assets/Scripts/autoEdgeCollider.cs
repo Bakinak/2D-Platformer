ec3c845ef09b86b6a3cbcfc63333fb825fded5f6
Bakinak <akobbe16@student.aau.dk>
2020-08-11 13:45:23 +0200

    more level and auto collider script

---
+++ 2D Platformer/Assets/Scripts/autoEdgeCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autoEdgeCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //WHY DOESN'T THIS WORK
+        float size = GetComponent<SpriteRenderer>().size.x;
+
+        Vector2[] autoPoints;
+        autoPoints = GetComponent<EdgeCollider2D>().points;
+        autoPoints[0].x = -size / 2;
+        autoPoints[1].x = size / 2;
+
+        GetComponent<EdgeCollider2D>().points = autoPoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
